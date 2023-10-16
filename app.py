b7e4cbdef65b78cd59bb74375871562e95ef4e54
AlpherOmondi <alphaomondi001@gmail.com>
2023-10-17 02:03:21 +0300

    Chore: Updated flask version

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask # Import flask package
 
-app = Flask(__name__)
+app = Flask(__name__) # Create an instance of Flask(Object)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
+@app.route('/') # Specify route
+def hello_world():  # put application's code here First Api function
     return 'Hello World!'
 
 
