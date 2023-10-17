180dbd974dae2c5a753917b175c6ed3ff4462311
AlpherOmondi <alphaomondi001@gmail.com>
2023-10-17 03:31:09 +0300

    feat: Added a function to get a single recipe

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import jsonify, Flask, request # These are packages needed.import
+from flask import jsonify, Flask, request  # These are packages needed.import
 # """
 # Jsonify is here to convert lists to json. Language translator
 # """
-from http import HTTPStatus # This enum includes different codes.
+from http import HTTPStatus  # This enum includes different codes.
 
-#Instance of flask
+# Instance of flask
 app = Flask(__name__)
 # Data
 recipes = [
@@ -21,7 +21,21 @@ recipes = [
     }
 
 ]
-@app.route('/recipes',methods=['GET']) # If we dont specify the default will be get
+
+
+@app.route('/recipes', methods=['GET'])  # If we dont specify the default will be get
 def get_recipes():
     return jsonify(recipes)
+
+
 # Get a specific item
+@app.route('/recipes/<int:recipe_id>', methods=['GET'])
+# The syntax means the value will be assigned to an integer variable.
+# The function will then loop through recipes to locate the recipe that has is.
+def get_recipe(recipe_id):
+    # recipe = next((recipe for recipe in recipes if recipe['id'] == recipe_id), None)
+    for recipe in recipes:
+        if recipes['id'] == recipe_id:
+            return jsonify(recipe)
+        else:
+            return jsonify({'message': 'recipe not found'}), HTTPStatus.NOT_FOUND
